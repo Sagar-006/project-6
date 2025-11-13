488db33d68f498ebc570e9609d203cd17b210473
Sagar-006 <sagarbiradar7030@gmail.com>
2025-11-14 01:01:35 +0530

    ...

--- app/api/streams/my/route.ts
+++ app/api/streams/my/route.ts
@@ -1,45 +1,7 @@
-import db from "@/app/lib/db";
-import { getSession } from "@/app/lib/session";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const session = await getSession();
-
-  if (!session?.user) {
-    return NextResponse.json(
-      {
-        message: "Unauthenticated",
-      },
-      {
-        status: 403,
-      }
-    );
-  }
-  const user = session.user;
-
-  const streams = await db.stream.findMany({
-    where: {
-      userId: user.id,
-    },
-    include: {
-      _count: {
-        select: {
-          upvotes: true,
-        },
-      },
-      upvotes: {
-        where: {
-          userId: user.id,
-        },
-      },
-    },
-  });
-
+export async function GET(){
   return NextResponse.json({
-    streams: streams.map(({ _count, ...rest }) => ({
-      ...rest,
-      upvotes: _count.upvotes,
-      haveUpvoted: rest.upvotes.length ? true : false,
-    })),
-  });
+    message:'Hi'
+  })
 }
