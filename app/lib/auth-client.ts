e6b948d25b458e4a4b2100b2a9bdefcbb5fa1188
Sagar-006 <sagarbiradar7030@gmail.com>
2025-12-02 15:36:34 +0530

    ..

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "https://vercel.com/sagarbiradar7030-1463s-projects/project-6",
+  baseURL: "https://project-6-eight-theta.vercel.app",
 });
